2813a528d49a454a9cf7c3dd925f8dd6a52d8e24
Thomas Pan <tpan@e1z2r8p25.42.us.org>
2016-12-08 21:43:12 -0800

    rewrote attempt place and added comments for clarity

--- src/solver.c
+++ src/solver.c
@@ -22,30 +22,35 @@ static void		reset_solution(char **grid)
 }
 
 /*
-** 
+ make america free again, used to free the grid in case of backtrack. Unsure
+ if this implementation can be used to recurse n levels or just all or none
 */
 
-int		attempt_place(t_etris *piece, char **tet_map, int col, int row)
+int		attempt_place(t_etris *piece, t_map *map, int x, int y)
 {
 	int i;
-	int colrow;
-	int **type;
+	int j;
+
 	i = 0;
-	colrow = (col * 10) + r;
-	type = tet->type;
-	while (i++ <4)
+	while (i < piece->width)
 	{
-		if ((type[i][1] < 0) && (col + type[i][1] < 0))
-			return (0);
-		if	(type[i] != '.')
-			return (0);
+		j = 0;
+		while (j < piece->height)
+		{
+			if (piece->x[j][i] == '#' && map->array[y + j][x + i] != '.')
+				return(0);
+			j++;
+		}
+		i++;
 	}
 	place_piece(piece, tet_map, colrow, t_etris->letter);
 	return (1);
 }
 
 /*
-**
+while within range of height and width, checks validity of placing at that spot
+if the position is occupied by a hash then we can our tetrimino there, however
+if there is anything other than a period than we cannot/should not.
 */
 
 static int		solve_grid(t_etris *piece, char **map, int map_size) 
@@ -66,7 +71,6 @@ static int		solve_grid(t_etris *piece, char **map, int map_size)
 					return(1);
 				coords->row = row;
 				coords->col = col;
-				//colrow = (col * 10) + row;
 				place_piece(piece, map, coords, '.');
 			}
 			col++;
